d59cd10fa76b941bb09128d7dc40318d3f410bde
Cheng Zhi Hong <s10205253@connect.np.edu.sg>
2020-12-27 02:20:30 +0800

    tried implementing the api

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-let url = "https://ghibliapi.herokuapp.com/films"
-
-
+var url = "https://ghibliapi.herokuapp.com/films"
 $(document).ready(function(){
-    //let movieData = GetMovieData();
-    //console.log(movieData);
     $("#userInput").keypress(function (e){
         if(e.which == 13){
             location.href = "test.html"
         }
     });
-})
-function GetMovieData(){
-    return fetch(url)
-    .then(response => response.json)
-    .then(data => {
-        return data;
+    var test = $("#userInput").val()
+    localStorage.setItem("title", test);
+    var movieTitle = localStorage.title
+    console.log(movieTitle);
+    //try sending the userinput to the local storage
+    fetch(url)
+    .then(response => response.json())
+    .then(function(data){
+        $("#1").html(data[0].title)
+        $("#2").html(data[0].description)
+        $("#3").html(data[0].people)
     })
-}
-function SendToPage(){
-    var input = $("#userInput")
-    console.log(input)
-}
+})
+
 
 
