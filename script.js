3e4f5918313896fca53740e5f81d40c917c74c74
Cheng Zhi Hong <s10205253@connect.np.edu.sg>
2020-12-27 23:00:03 +0800

    switched to select bar

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 var url = "https://ghibliapi.herokuapp.com/films"
 $(document).ready(function(){
-    $("#userInput").keypress(function (e){
-        if(e.which == 13){
-            location.href = "test.html"
-        }
-    });
-    var test = $("#userInput").val()
-    localStorage.setItem("title", test);
-    var movieTitle = localStorage.title
+  $("#submit").click(function(){
+      location.href = "test.html"
+      localStorage.setItem("title", $("#userInput").val());
+      console.log($("#userInput").val());
+  })
+    var movieTitle = localStorage.title;
     console.log(movieTitle);
-    //try sending the userinput to the local storage
     fetch(url)
     .then(response => response.json())
     .then(function(data){
-        $("#1").html(data[0].title)
-        $("#2").html(data[0].description)
-        $("#3").html(data[0].people)
+        console.log(data);
+        for (i = 0; i < data.length; i++){
+            var option = document.createElement("option");
+            option.value = data[i].title;
+            option.text = data[i].title;
+            $("#userInput").append(option);
+            if (data[i].title == movieTitle){
+                $("#1").html(data[i].title);
+                $("#2").html(data[i].description);
+                $("#3").html(data[i].director);
+            }
+        }
     })
 })
 
